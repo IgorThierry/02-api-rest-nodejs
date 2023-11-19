e243130c7eff27f96f7611f2cd5085ce1354446d
Igor Thierry <igorthierry15@gmail.com>
2023-11-19 16:06:09 -0300

    add getTransaction endpoint

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,9 +5,25 @@ import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
-    const transactions = await knex('transactions').select('*')
+    const transactions = await knex('transactions').select()
 
     return { transactions }
+  })
+
+  app.get('/:id', async (request, reply) => {
+    const getTransactionSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    if (!transaction) {
+      return reply.status(404).send({ message: 'Transaction not found' })
+    }
+
+    return { transaction }
   })
 
   app.post('/', async (request, reply) => {
