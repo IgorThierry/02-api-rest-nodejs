b35f50f6a552671b772aa34d1729043cd8f2f2a9
Igor Thierry <igorthierry15@gmail.com>
2024-04-01 19:31:09 -0300

    chore: Update package.json and server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { env } from './env'
 import { app } from './app'
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log(`Server started on http://localhost:${env.PORT}`)
-})
+app
+  .listen({
+    port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+  })
+  .then(() => {
+    console.log(`Server started on http://localhost:${env.PORT}`)
+  })
